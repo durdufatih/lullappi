872a62174e255a9c1c1c343ab2ab70b23aec2030
Fatih Durdu <mfdurdu@gmail.com>
2023-02-16 17:37:16 +0300

    Initial Commit

---
+++ lullappi/lullappiApp.swift
@@ -0,0 +1,17 @@
+//
+//  lullappiApp.swift
+//  lullappi
+//
+//  Created by Mehmet Fatih Durdu on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct lullappiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
